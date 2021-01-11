e1cc47916178fc2a89245d6b39c50066b1ed1607
alissonFord <alicjaszlachta@outlook.com>
2021-01-11 20:18:20 +0100

    method for displaying covered word added

--- hangman.js
+++ hangman.js
@@ -1,7 +1,12 @@
 const alphabet = document.getElementById('letters');
+const word = document.getElementById('word');
+var secretWord = '';
+var counter = 10;
 console.log(alphabet);
 
 const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'w', 'x', 'y', 'z'];
+
+const secretWords = ['ignorance', 'double', 'disclose', 'experiment', 'depart', 'voucher', 'aloof', 'road', 'gas'];
 
 function displayLetters() {
     let list = document.createElement('ul');
@@ -14,7 +19,24 @@ function displayLetters() {
     }
 }
 
+function pickSecretWord() {
+    let index = Math.floor(Math.random() * Math.floor(secretWords.length));
+    secretWord = secretWords[index];
+    console.log(secretWord);
+}
+
+function coverSecretWord() {
+    for (let i = 0; i < secretWord.length; i++) {
+        console.log('inside voersecretword');
+        let currentLetter = document.createElement('span');
+        currentLetter.innerHTML = ' _ ';
+        word.appendChild(currentLetter);
+    }
+}
+
 function checkLetter() {}
 
 
 displayLetters();
+pickSecretWord();
+coverSecretWord();
